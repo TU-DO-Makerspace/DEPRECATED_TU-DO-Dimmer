74077bbeeaebdb93362c5baa8654fc57848c1330
Patrick Pedersen <ctx.xda@gmail.com>
2020-03-06 09:37:39 +0100

    PatchIndicator: Oops
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- src/PatchIndicator.cpp
+++ src/PatchIndicator.cpp
@@ -28,13 +28,6 @@ PatchIndicator::PatchIndicator() {
 
 PatchIndicator::PatchIndicator(bool config, uint8_t common, uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t f, uint8_t g) :
 _config(config), _common(common) {
-    pinMode(a, OUTPUT);
-    pinMode(b, OUTPUT);
-    pinMode(c, OUTPUT);
-    pinMode(d, OUTPUT);
-    pinMode(e, OUTPUT);
-    pinMode(f, OUTPUT);
-
     _segments[0] = a;
     _segments[1] = b;
     _segments[2] = c;
